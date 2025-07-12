e32993f30e23c2dc3e8d77fe7ebb9e8f3ea7a945
bimbim1214 <bimoadityapangestu@gmail.com>
2025-07-12 17:53:37 +0700

    manambahkan API buah

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,15 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BuahController;
 
 
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/tes', function () {
-    return response()->json(['message' => 'route tes OK']);
-});
+Route::get('/buah', [BuahController::class, 'index']);
+Route::post('/buah', [BuahController::class, 'store']);
+Route::get('/buah/{id}', [BuahController::class, 'show']);
+Route::put('/buah/{id}', [BuahController::class, 'update']);
+Route::delete('/buah/{id}', [BuahController::class, 'destroy']);
